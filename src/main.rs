0f73501ce6d3a122da7e1e49414edf3c51b417a5
Julian Goldsmith <julian@juliangoldsmith.com>
2017-05-19 16:36:16 -0400

    work with bytes instead of chars

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,22 @@ use bitstream::Bitstream;
 #[derive(Debug)]
 struct Node {
     count: usize,
-    val: Option<char>,
+    val: Option<u8>,
     left: Option<Box<Node>>,
     right: Option<Box<Node>>,
 }
 
-fn build_freq_list(data: &String) -> Vec<Node> {
+fn build_freq_list(data: &Vec<u8>) -> Vec<Node> {
     let mut nodes: Vec<Node> = (0..256).
         map(|i| Node { 
             count: 0, 
-            val: Some(i as u8 as char),
+            val: Some(i as u8),
             left: None, 
             right: None 
         }).
         collect();
 
-    for idx in data.chars().map(|c| c as usize) {
+    for idx in data.iter().map(|c| *c as usize) {
         nodes[idx].count += 1
     }
 
@@ -51,11 +51,11 @@ fn build_tree_internal(mut nodes: Vec<Node>) -> Node {
     }
 }
 
-fn build_tree(data: &String) -> Node {
+fn build_tree(data: &Vec<u8>) -> Node {
     build_tree_internal(build_freq_list(data))
 }
 
-fn encode_char_internal(root: &Node, c: char, acc: Bitstream) -> Option<Bitstream> {
+fn encode_char_internal(root: &Node, c: u8, acc: Bitstream) -> Option<Bitstream> {
     match (root.val, &root.left, &root.right) {
         (Some(val), _, _) if val == c => Some(acc),
         (None, &Some(ref left), &Some(ref right)) =>
@@ -67,11 +67,11 @@ fn encode_char_internal(root: &Node, c: char, acc: Bitstream) -> Option<Bitstrea
     }
 }
 
-fn encode_char(root: &Node, c: char) -> Option<Bitstream> {
+fn encode_char(root: &Node, c: u8) -> Option<Bitstream> {
     encode_char_internal(root, c, Bitstream::new())
 }
 
-fn decode_char_internal(root: &Node, mut s: Bitstream) -> (Option<char>, Bitstream) {
+fn decode_char_internal(root: &Node, mut s: Bitstream) -> (Option<u8>, Bitstream) {
     match (root.val, &root.left, &root.right) {
         (Some(val), _, _) => (Some(val), s),
         (None, &Some(ref left), &Some(ref right)) =>
@@ -84,18 +84,18 @@ fn decode_char_internal(root: &Node, mut s: Bitstream) -> (Option<char>, Bitstre
     }
 }
 
-fn decode_bitstream_internal(root: &Node, s: Bitstream, mut acc: String) -> Option<String> {
+fn decode_bitstream_internal(root: &Node, s: Bitstream, mut acc: Vec<u8>) -> Option<Vec<u8>> {
     match decode_char_internal(root, s) {
         (Some(c), ns) => { acc.push(c); decode_bitstream_internal(root, ns, acc) },
         (None, _) => Some(acc),
     }
 }
 
-fn decode_bitstream(root: &Node, s: Bitstream) -> Option<String> {
-    decode_bitstream_internal(root, s.reverse(), String::from(""))
+fn decode_bitstream(root: &Node, s: Bitstream) -> Option<Vec<u8>> {
+    decode_bitstream_internal(root, s.reverse(), Vec::new())
 }
 
-fn get_test_data() -> String {
+fn get_test_data() -> Vec<u8> {
     let path = Path::new("../testfile.txt");
     let display = path.display();
 
@@ -105,11 +105,10 @@ fn get_test_data() -> String {
         Ok(file) => file,
     };
 
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display,
-                                                   why.description()),
-        Ok(_) => s,
+    let mut bytes: Vec<u8> = Vec::new();
+    match file.read_to_end(&mut bytes) {
+        Ok(_) => bytes,
+        Err(e) => panic!("Error reading file: {}", e),
     }
 }
 
@@ -117,7 +116,9 @@ fn main() {
     let data = get_test_data();
     let root = build_tree(&data);
 
-    let enc = data.chars().map(|c| encode_char(&root, c).unwrap()).fold(Bitstream::new(), |acc, x| acc + x);
+    let enc = data.iter().
+        map(|c| encode_char(&root, *c).unwrap()).
+        fold(Bitstream::new(), |acc, x| acc + x);
 
     let dec = decode_bitstream(&root, enc);
     println!("{:?}", dec)

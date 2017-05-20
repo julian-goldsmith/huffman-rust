217b6c7716c48a0a3b3154e647ef29dc7654e343
Julian Goldsmith <julian@juliangoldsmith.com>
2017-05-20 17:40:10 -0400

    wip

--- src/huffman.rs
+++ src/huffman.rs
@@ -1,60 +1,122 @@
-use std::mem;
-use std::ptr;
+use std;
+use std::io;
+use std::io::Write;
 use bitstream::Bitstream;
 
 #[derive(Debug)]
 pub struct Node {
     count: usize,
-    val: Option<u8>,
+    val: Option<u16>,
     left: Option<Box<Node>>,
     right: Option<Box<Node>>,
 }
 
-fn build_freq_list(data: &Vec<u8>) -> Vec<Node> {
-    let mut nodes: Vec<Node> = (0..256).
-        map(|i| Node { 
-            count: 0, 
-            val: Some(i as u8),
-            left: None, 
-            right: None 
-        }).
-        collect();
+fn build_freq_list(data: &Vec<u16>) -> Vec<Box<Node>> {
+    let mut nodes: Vec<Box<Node>> = Vec::new();
 
-    for idx in data.iter().map(|c| *c as usize) {
-        nodes[idx].count += 1
-    }
+    for i in 0..65536 {
+        nodes.push(Box::new(
+            Node { 
+                count: 0, 
+                val: Some(i as u16),
+                left: None, 
+                right: None 
+            }));
+    };
+
+    for code in data.iter() {
+        nodes[*code as usize].count += 1;
+    };
 
     nodes
 }
 
-fn build_tree_internal(mut nodes: Vec<Node>) -> Node {
-    // FIXME: can we just insert the node instead of sorting every time?
-    nodes.sort_by(|a, b| b.count.cmp(&a.count));
-
-    let lo = nodes.pop();
-    let ro = nodes.pop();
-
-    match (lo, ro) {
-        (Some(left), None) => left,
-        (Some(left), Some(right)) => {
-            nodes.push(Node {
-                count: left.count + right.count,
-                val: None,
-                left: Some(Box::new(left)),
-                right: Some(Box::new(right)),
-            });
-            build_tree_internal(nodes)
-        },
-        _ => panic!("Must have nodes to build_tree"),
+fn clamp(val: usize, min: usize, max: usize) -> usize {
+    if val > max {
+        max
+    } else if val < min {
+        min
+    } else {
+        val
     }
 }
 
-pub fn build_tree(data: &Vec<u8>) -> Node {
+fn find_pos(nodes: &Vec<Box<Node>>, node: &Box<Node>) -> Option<usize> {
+    // same as nodes.iter().position(|other| other.count > node.count)
+    
+    if nodes.len() == 0 {
+        return Some(0)
+    }
+
+    let mut lower = 0;
+    let mut upper = nodes.len() - 1;
+
+    loop {
+        if nodes[lower].count == node.count {
+            return Some(lower)
+        }
+
+        if lower == upper {
+            return None
+        }
+
+        let newlower = clamp(lower * 2 + 1, lower, upper);
+        if nodes[lower].count > node.count {
+            upper = newlower;
+        } else {
+            lower = newlower;
+        }
+
+        let newupper = clamp(upper / 2, lower, upper);
+        if nodes[upper].count > node.count {
+            lower = newupper;
+        } else {
+            upper = newupper;
+        }
+    }
+}
+
+fn build_tree_internal(mut nodes: Vec<Box<Node>>) -> Box<Node> {
+    // FIXME: can we just insert the node instead of sorting every time?
+
+    let mut count = 0;
+
+    loop {
+        let lo = nodes.pop();
+        let ro = nodes.pop();
+
+        match (lo, ro) {
+            (Some(left), None) => { println!("Exiting build_tree_internal"); return left; },
+            (Some(left), Some(right)) => {
+                count += 1;
+
+                if count % 256 == 0 {
+                    println!("Count {}", count);
+                };
+
+                let node = Box::new(Node {
+                    count: left.count + right.count,
+                    val: None,
+                    left: Some(left),
+                    right: Some(right),
+                });
+                let idx = match find_pos(&nodes, &node) {
+                    Some(idx) => idx,
+                    None => nodes.len(),
+                };
+                nodes.insert(idx, node);
+            },
+            _ => panic!("Must have nodes to build_tree"),
+        };
+    };
+}
+
+pub fn build_tree(data: &Vec<u16>) -> Box<Node> {
     let freq_list = build_freq_list(data);
     build_tree_internal(freq_list)
 }
 
-fn encode_char_internal(root: &Node, c: u8, acc: Bitstream) -> Option<Bitstream> {
+fn encode_char_internal(root: &Node, c: u16, acc: Bitstream) -> Option<Bitstream> {
     match (root.val, &root.left, &root.right) {
         (Some(val), _, _) if val == c => Some(acc),
         (None, &Some(ref left), &Some(ref right)) =>
@@ -66,11 +128,22 @@ fn encode_char_internal(root: &Node, c: u8, acc: Bitstream) -> Option<Bitstream>
     }
 }
 
-fn encode_char(root: &Node, c: u8) -> Option<Bitstream> {
+fn encode_char(root: &Node, c: u16) -> Option<Bitstream> {
     encode_char_internal(root, c, Bitstream::new())
 }
 
-fn decode_char(root: &Node, mut s: Bitstream) -> (Option<u8>, Bitstream) {
+pub fn precalc_bitstreams(root: &Node) -> Result<Vec<Bitstream>,()> {
+    let mut calc: Vec<Bitstream> = Vec::new();
+    for i in 0..65536 {
+        match encode_char(root, i as u16) {
+            Some(item) => calc.push(item),
+            None => return Err(()),
+        }
+    };
+    Ok(calc)
+}
+
+fn decode_char(root: &Node, mut s: Bitstream) -> (Option<u16>, Bitstream) {
     match (root.val, &root.left, &root.right) {
         (Some(val), _, _) => (Some(val), s),
         (None, &Some(ref left), &Some(ref right)) =>
@@ -83,27 +156,13 @@ fn decode_char(root: &Node, mut s: Bitstream) -> (Option<u8>, Bitstream) {
     }
 }
 
-fn decode_bitstream_internal(root: &Node, s: Bitstream, mut acc: Vec<u8>) -> Option<Vec<u8>> {
+fn decode_bitstream_internal(root: &Node, s: Bitstream, mut acc: Vec<u16>) -> Option<Vec<u16>> {
     match decode_char(root, s) {
         (Some(c), ns) => { acc.push(c); decode_bitstream_internal(root, ns, acc) },
         (None, _) => Some(acc),
     }
 }
 
-pub fn decode_bitstream(root: &Node, s: Bitstream) -> Option<Vec<u8>> {
+pub fn decode_bitstream(root: &Node, s: Bitstream) -> Option<Vec<u16>> {
     decode_bitstream_internal(root, s.reverse(), Vec::new())
 }
-
-pub fn precalc_bitstreams(root: &Node) -> Result<[Bitstream; 256],()> {
-    unsafe {
-        let mut calc: [Bitstream; 256] = mem::uninitialized();
-        for i in 0..256 {
-            match encode_char(root, i as u8) {
-                Some(item) => ptr::write(&mut calc[i], item),
-                None => return Err(()),
-            }
-        };
-        Ok(calc)
-    }
-}
-
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use bitstream::Bitstream;
+use std::io::Write;
 
 fn read_file(path: &String) -> Vec<u8> {
     let path = Path::new(path);
@@ -26,25 +27,30 @@ fn read_file(path: &String) -> Vec<u8> {
 
 fn main() {
     let data = read_file(&String::from("../testfile.txt"));
-    let enc = lzw::encode(&data);
-    println!("enc {:?}", enc);
 
-    let dec = lzw::decode(&enc);
-    println!("dec {:?}", dec);
 
-    /*
-    let root = huffman::build_tree(&data);
+    let lz_enc = lzw::encode(&data);
+    println!("lzw::encode finished");
+    std::io::stdout().flush().ok().expect("error");
+
+    let root = huffman::build_tree(&lz_enc);
+    println!("huffman::build_tree finished");
+    std::io::stdout().flush().ok().expect("error");
 
     let calc = match huffman::precalc_bitstreams(&root) {
         Ok(calc) => calc,
         Err(_) => panic!("Couldn't precalc bitstream"),
     };
+    println!("huffman::precalc_bitstreams finished");
+    std::io::stdout().flush().ok().expect("error");
 
-    let enc = data.iter().
+    let huff_enc = lz_enc.iter().
         map(|c| &calc[*c as usize]).
         fold(Bitstream::new(), |acc, x| acc + x);
 
-    let dec = huffman::decode_bitstream(&root, enc);
-    println!("{:?}", dec)
-    */
+
+    let huff_dec = huffman::decode_bitstream(&root, huff_enc).unwrap();
+    let lz_dec = lzw::decode(&huff_dec);
+
+    println!("{:?}", lz_dec)
 }
